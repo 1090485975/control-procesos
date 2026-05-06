f69600c7b8980b75adbb43df7dfd9b502710ef1b
Luis Daniel Trejos Teheran <abg.danielteheran@gmail.com>
2026-05-06 11:35:05 -0500

    Crear ruta /nuevo

---
+++ app/nuevo/page.tsx
@@ -0,0 +1,8 @@
+export default function NuevoPage() {
+  return (
+    <main style={{ padding: "24px" }}>
+      <h1>Nuevo proceso</h1>
+      <p>La ruta /nuevo ya está funcionando.</p>
+    </main>
+  );
+}
